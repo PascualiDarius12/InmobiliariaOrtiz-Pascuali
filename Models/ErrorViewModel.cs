48e0e2d7a1a924bf2137503c7fa210789db74f99
PascualiDarius12 <dario12bifa@gmail.com>
2024-03-18 13:16:25 -0300

    app .net

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InmobiliariaOrtiz_Pascuali.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
