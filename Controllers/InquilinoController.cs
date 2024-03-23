77df9861b6a83dbefb46e07b27fdac5cbaf478dd
PascualiDarius12 <dario12bifa@gmail.com>
2024-03-23 13:31:42 -0300

    controlador inquilino

--- Controllers/InquilinoController.cs
+++ Controllers/InquilinoController.cs
@@ -13,10 +13,60 @@ public class InquilinoController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+   public IActionResult Index()
     {
-        return View();
+        InquilinoRepo pr = new InquilinoRepo();
+        var listaInquilino = pr.getInquilino();
+        return View(listaInquilino);
     }
+    public IActionResult editar(int id)
+    {
+        //logica para mostrar el formulario vacio o lleno con el propietario que queremos editar
+        if (id > 0)
+        {
+            InquilinoRepo repo = new InquilinoRepo();
+            var inquilino = repo.buscarInquilino(id);
+            return View(inquilino);
+        }
+        else
+        {
+            return View();
+        }
+
+
+    }
+    public IActionResult Insertar(Inquilino inquilino)
+    {
+        InquilinoRepo repo = new InquilinoRepo();
+        if (inquilino.IdInquilino > 0)
+        {
+            repo.Modificar(inquilino);
+
+        }
+        else
+        {
+            repo.Insertar(inquilino);
+        }
+
+
+
+        return RedirectToAction(nameof(Index));
+    }
+
+
+
+    [HttpPost]
+    public IActionResult Eliminar(int id)
+    {
+        InquilinoRepo repo = new InquilinoRepo();
+        var resultado = repo.Eliminar(id);
+        if (resultado == -1)
+        {
+            TempData["Error"] = "Ocurrió un error al eliminar el propietario.";
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
 
 
 
