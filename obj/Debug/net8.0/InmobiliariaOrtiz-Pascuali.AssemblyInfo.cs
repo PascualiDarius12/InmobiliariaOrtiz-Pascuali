539fc9a87cf4862b10ac2b065d6c4b87ffb6db8e
PascualiDarius12 <dario12bifa@gmail.com>
2024-04-07 15:30:28 -0300

    vista

--- obj/Debug/net8.0/InmobiliariaOrtiz-Pascuali.AssemblyInfo.cs
+++ obj/Debug/net8.0/InmobiliariaOrtiz-Pascuali.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobiliariaOrtiz-Pascuali")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3a0d48d2736c8cb478fea3390ad39ddd487b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a6e6693afb12960462180c8143f6e5d4935fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobiliariaOrtiz-Pascuali")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobiliariaOrtiz-Pascuali")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
