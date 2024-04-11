7229fc551433ce8e61698aaa0bb2c43770871791
PascualiDarius12 <dario12bifa@gmail.com>
2024-04-11 15:56:29 -0300

    bd

--- bd/inmobiliariaortiz_pascuali.sql
+++ bd/inmobiliariaortiz_pascuali.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-04-2024 a las 20:44:33
+-- Tiempo de generación: 11-04-2024 a las 20:55:52
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
--- obj/Debug/net8.0/InmobiliariaOrtiz-Pascuali.AssemblyInfo.cs
+++ obj/Debug/net8.0/InmobiliariaOrtiz-Pascuali.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobiliariaOrtiz-Pascuali")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c410210443e12058c50fd71e66b5e5c119826af4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dfd1178f9bb44b69afff603008e32c26bea223")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobiliariaOrtiz-Pascuali")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobiliariaOrtiz-Pascuali")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
