b464bb2eded6fcb0ce3c606774cf00c0af746f0b
PascualiDarius12 <dario12bifa@gmail.com>
2024-04-15 19:35:59 -0300

    program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//configurar servicio de autenticacion
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>//el sitio web valida con cookie
+    {
+        options.LoginPath = "/Usuarios/Login";
+        options.LogoutPath = "/Usuarios/Logout";
+        options.AccessDeniedPath = "/Home/Restringido";
+        //options.ExpireTimeSpan = TimeSpan.FromMinutes(5);//Tiempo de expiración
+    });
+
+//crear politicas de autorizacion
+
+builder.Services.AddAuthorization(options =>
+{
+
+    options.AddPolicy("Administrador", policy => policy.RequireRole("Administrador"));
+    //no hace falta empleado por que si esta autenticado y no es administrados seria empleado, salvo que tengamos mas roles
+    //options.AddPolicy("Empleado", policy => policy.RequireRole("Empleado"));
+});
 
 var app = builder.Build();
 
@@ -13,12 +32,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(); // archivos como fotos, css etc
 
 app.UseRouting();
 
+// Habilitar autenticación
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
